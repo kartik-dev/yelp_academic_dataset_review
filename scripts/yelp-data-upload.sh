15911704d54593c987091b12314221b51c756e99
KARTIK RAMALINGAM <KARTIK RAMALINGAM>
2016-10-26 19:39:29 +0200

    first commit

---
+++ scripts/yelp-data-upload.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Cleanup....."
+
+rm -rf /tmp/yelp-dataset
+
+if [ "$1" = "" ]
+then
+  echo "Please specify the tar file path"
+  exit 1
+fi
+
+echo $1
+
+echo "Setup tmp directory to extract the dataset"
+
+mkdir /tmp/yelp-dataset
+
+echo " Extract tar file"
+
+tar xvf $1 -C /tmp/yelp-dataset/
+
+echo "Creating HDFS directory"
+
+hdfs dfs -mkdir /yelp-dataset
+
+echo "Uploading files to HDFS"
+
+hdfs dfs -copyFromLocal /tmp/yelp-dataset/*.json /yelp-dataset
+
